0bafe0dda75507def70f8b1a2b4138065def96db
Joao Meyer <1994meyer@gmail.com>
2018-06-26 21:52:21 -0300

    Backing up

--- main_window_ui.py
+++ main_window_ui.py
@@ -1,17 +1,19 @@
 import re
 
 from copy import deepcopy
+from functools import wraps
+from pprint import pprint
 
 from layout import Ui_MainWindow
-from PyQt5.QtWidgets import (
-    QMainWindow, QListWidgetItem)
+from PyQt5.QtWidgets import QMainWindow, QListWidgetItem
 from PyQt5.QtCore import Qt
-#
 
 from grammar.grammar import Grammar
 from grammar.production import Prod
 
-GRAMMAR_PATTERN = re.compile(r"^[A-Z]'?->[a-z0-9&][A-Z]?(\|[a-z0-9&][A-Z]?)*$")
+SPACE_REMOVE_RE = re.compile(r' +')
+NON_TERMINAL_RE = re.compile(r'[A-Z][\d]*')
+TERMINAL_RE = re.compile(r'[a-z\d]+')
 
 '''
 S -> A B
@@ -64,59 +66,151 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.buttonRemoveDirectRec.clicked.connect(self._remove_direct_recursion)
         self.buttonRemoveIndirectRec.clicked.connect(self._remove_indirect_recursion)
 
+    def check_name(fun):
+        @wraps(fun)
+        def wrapped(self, *args, **kwargs):
+            if self.lineInputGrammarName.text() == '':
+                self.log('There is no name for grammar')
+                return
+            return fun(self)
+        return wrapped
+
+    def check_factor(fun):
+        @wraps(fun)
+        def wrapped(self, *args, **kwargs):
+            num = self.lineEditFactorSteps.text().strip()
+            try:
+                num = int(num)
+                assert num >= 0
+            except Exception as e:
+                self.log('Steps is not a positive number')
+                return
+            return fun(self)
+        return wrapped
+
+    def has_grammar(fun):
+        @wraps(fun)
+        def wrapped(self, *args, **kwargs):
+            if self.selected_grammar == None:
+                self.log('No grammar selected')
+                return
+            return fun(self)
+        return wrapped
+
+    def check_grammar(fun):
+        @wraps(fun)
+        def wrapped(self, *args, **kwargs):
+            text = self.textEditGrammarInput.toPlainText()
+            table = self._filter_input(text)
+
+            # Check non-terminals
+            for non_terminal in table.keys():
+                if not NON_TERMINAL_RE.match(non_terminal):
+                    self.log(f'Invalid non-terminal: {non_terminal}')
+                    return
+
+            # Check productions
+            for expressions in table.values():
+                for exp in expressions:
+                    print(exp)
+                    if exp == Grammar.EPSILON:
+                        continue
+                    for symbol in exp.split():
+                        if not self._valid_symbol(symbol):
+                            self.log(f'Invalid expression: {exp}')
+                            return
+
+            return fun(self)
+        return wrapped
+
+    def _valid_symbol(self, symbol):
+        return re.match(NON_TERMINAL_RE, symbol) or re.match(TERMINAL_RE, symbol)
+
+    def _filter_input(self, input_str):
+        lines = input_str.split('\n')
+
+        # Filter empty lines
+        for line in list(lines):
+            if line == '':
+                lines.remove(line)
+
+        table = {}
+
+        # Get non-terminals
+        for line in lines:
+            line = re.sub(SPACE_REMOVE_RE, ' ', line).strip()
+            non_terminal = line.split('->')[0].strip()
+            expressions = line.split('->')[1].split('|')
+            table[non_terminal] = expressions
+
+        pprint(table)
+
+        return table
+
     def _save_grammar(self, name, grammar):
         item = QListWidgetItem(name)
         item.setData(Qt.UserRole, grammar)
         self.grammarList.addItem(item)
 
+    @has_grammar
     def _first(self):
         first = self.selected_grammar.first_sets()
         self.log('First {')
         self.log_dict(first)
         self.log('}')
 
+    @has_grammar
     def _first_nt(self):
         first_nt = self.selected_grammar.first_NT()
         self.log('First-NT {')
         self.log_dict(first_nt)
         self.log('}')
 
+    @has_grammar
     def _follow(self):
         follow = self.selected_grammar.follow_sets()
         self.log('Follow {')
         self.log_dict(follow)
         self.log('}')
 
+    @has_grammar
     def _nf(self):
         nf = self.selected_grammar.productive()
         self.log(f'Productive:\t{nf}')
 
+    @has_grammar
     def _vi(self):
         grammar = self.selected_grammar
         vi = grammar.reachable(grammar.start)
         self.log(f'Reachable:\t{vi}')
 
+    @has_grammar
     def _na(self):
         na = self.selected_grammar.simple_table()
         self.log('NA {')
         self.log_dict(na)
         self.log('}')
 
+    @has_grammar
     def _ne(self):
         ne = self.selected_grammar._epsilon_closure()
         ne = { i[0] for i in ne }
         self.log(f'Epsilon closure:\t{ne}')
 
+    @has_grammar
     def _empty(self):
         is_empty = self.selected_grammar.is_empty()
         self.log(f'Empty:\t{is_empty}')
 
+    @has_grammar
     def _finite(self):
         is_finite = self.selected_grammar.is_finite()
         self.log(f'Finite:\t{is_finite}')
 
+    @has_grammar
+    @check_factor
     def _factor(self):
-        steps = int(self.lineEditFactorSteps.text())
+        steps = int(self.lineEditFactorSteps.text().strip())
 
         if steps == 0:
             factored = self.selected_grammar.is_factored()
@@ -124,6 +218,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             factored = isinstance(self.selected_grammar.factor(steps), Grammar)
         self.log(f'Factor ({steps}):\t{factored}')
 
+    @has_grammar
     def _remove_direct_recursion(self):
         grammar = deepcopy(self.selected_grammar)
         grammar_name = f'{self.lineInputGrammarName.text()}_direct'
@@ -136,6 +231,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.log_grammar(grammar_name, grammar)
         self._save_grammar(grammar_name, grammar)
 
+    @has_grammar
     def _check_recursion(self):
         grammar = self.selected_grammar
         direct = grammar.has_direct_left_recursion()
@@ -143,6 +239,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.log(f'Non-terminals with direct recursion:\t{direct}')
         self.log(f'Non-terminals with indirect recursion:\t{indirect}')
 
+    @has_grammar
     def _remove_indirect_recursion(self):
         grammar = deepcopy(self.selected_grammar)
         grammar_name = f'{self.lineInputGrammarName.text()}_indirect'
@@ -153,7 +250,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.log_grammar(grammar_name, grammar)
         self._save_grammar(grammar_name, grammar)
 
-
+    @has_grammar
     def _to_proper(self):
         grammar_name = self.selected_grammar_name
 
@@ -187,6 +284,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self._na()
         self._ne()
 
+    @check_name
     def _create_grammar(self):
         grammar_name = self.lineInputGrammarName.text()
         grammar = self._to_grammar()
@@ -211,38 +309,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.lineInputGrammarName.setText(grammar_name)
         self.textEditGrammarInput.setText(str(grammar))
 
+    @check_grammar
     def _to_grammar(self):
         grammar_text = self.textEditGrammarInput.toPlainText()
-        lines = grammar_text.split('\n')
+        table = self._filter_input(grammar_text)
 
         terminals = set()
         productions = set()
-        non_terminals = set()
+        non_terminals = set(table.keys())
 
         # Start
-        start = lines[0].split('->')[0].strip()
-
-        # Non terminals
-        for line in lines:
-            non_terminal = line.split('->')[0].strip()
-            non_terminals.add(non_terminal)
+        start = grammar_text.split('\n')[0].split('->')[0].strip()
 
         # Terminals
-        for line in lines:
-            expressions = line.split('->')[1].split('|')
-            for exp in expressions:
-                symbols = filter(lambda i: i not in non_terminals, exp.split())
-                for symbol in symbols:
+        for expression in table.values():
+            for symbol in expression:
+                if symbol == Grammar.EPSILON:
+                    continue
+
+                if symbol not in non_terminals:
                     terminals.add(symbol)
 
         # Productions
-        for line in lines:
-            non_terminal = line.split('->')[0].strip()
-            expressions = line.split('->')[1].split('|')
-            for exp in expressions:
-                prod = exp.split()
-                new_prod = Prod(non_terminal, tuple(prod))
-                productions.add(new_prod)
+        for nt, prods in table.items():
+            for exp in prods:
+                productions.add(Prod(nt, tuple(exp)))
 
         return Grammar(non_terminals, terminals, productions, start)
 
@@ -255,7 +346,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def log_grammar(self, name, grammar):
         self.log(f'Name:\t{name}')
-        self.log(f'Start:\t{grammar.start}')
         self.log(f'Vnt:\t{str(grammar.non_terminals)}')
         self.log(f'Vt:\t{str(grammar.terminals)}')
-        self.log(f'P:\t{str(grammar.productions)}')
+        self.log(str(grammar))
